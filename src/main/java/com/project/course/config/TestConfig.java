2418d790a049335cf3d846dfb2a1a2186d307012
Gabriel Birck <birckzin@gmail.com>
2024-11-05 15:19:57 -0300

    correções no order

--- src/main/java/com/project/course/config/TestConfig.java
+++ src/main/java/com/project/course/config/TestConfig.java
@@ -1,12 +1,15 @@
 package com.project.course.config;
 
+import com.project.course.entities.Order;
 import com.project.course.entities.User;
+import com.project.course.repositories.OrderRepository;
 import com.project.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 @Configuration
@@ -16,12 +19,20 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private OrderRepository orderRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
         User u1 = new User(null, "Joao Kleber", "Joaozinho@gmail.com", "54991204170", "123");
         User u2 = new User(null, "Betinho Tilt", "Betao@gmail.com", "54991111111", "123");
 
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:09Z"), u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+
         userRepository.saveAll(Arrays.asList(u1, u2));
+        orderRepository.saveAll(Arrays.asList(o1, o2, o3));
     }
 }
