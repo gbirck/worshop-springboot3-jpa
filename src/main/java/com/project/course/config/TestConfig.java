1f8210cae9a677b1d9beab8cd3b248b751cdb503
Gabriel Birck <birckzin@gmail.com>
2024-11-05 16:05:42 -0300

    relacionamento muitos para muitos

--- src/main/java/com/project/course/config/TestConfig.java
+++ src/main/java/com/project/course/config/TestConfig.java
@@ -45,13 +45,19 @@ public class TestConfig implements CommandLineRunner {
 
         Category c1 = new Category(null, "Electronics");
         Category c2 = new Category(null, "Books");
-        Category c3 = new Category(null, "Vegetables");
+        Category c3 = new Category(null, "Computers");
 
         Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
         Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
         Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
         Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
         Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
+        p1.getCategories().add(c2);
+        p2.getCategories().add(c1);
+        p3.getCategories().add(c3);
+        p4.getCategories().add(c3);
+        p5.getCategories().add(c2);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
