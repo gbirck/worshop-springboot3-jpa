8a557880a5d0f7278477794df492e20dcc42704e
Gabriel Birck <birckzin@gmail.com>
2024-11-05 15:33:01 -0300

    adicionado enum orderStatus

--- src/main/java/com/project/course/config/TestConfig.java
+++ src/main/java/com/project/course/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.project.course.config;
 
 import com.project.course.entities.Order;
 import com.project.course.entities.User;
+import com.project.course.entities.enums.OrderStatus;
 import com.project.course.repositories.OrderRepository;
 import com.project.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Joao Kleber", "Joaozinho@gmail.com", "54991204170", "123");
         User u2 = new User(null, "Betinho Tilt", "Betao@gmail.com", "54991111111", "123");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:09Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:09Z"), OrderStatus.SHIPPED, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.CANCELLED, u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
---
+++ src/main/java/com/project/course/entities/enums/OrderStatus.java
@@ -0,0 +1,32 @@
+package com.project.course.entities.enums;
+
+import org.aspectj.weaver.reflect.IReflectionWorld;
+
+public enum OrderStatus {
+
+    WAITING_PAYMENT(1),
+    PAID(2),
+    PACKING(3),
+    SHIPPED(4),
+    DELIVERED(5),
+    CANCELLED(6);
+
+    private int code;
+
+    private OrderStatus(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public static OrderStatus valueOf(int code) {
+        for (OrderStatus value : OrderStatus.values()) {
+            if (value.getCode() == code) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("No OrderStatus with code " + code);
+    }
+}
