988c49c655be757308fb3849e156e2a41b8296d9
Gabriel Birck <birckzin@gmail.com>
2024-11-06 15:54:09 -0300

    adicionado update de user

--- src/main/java/com/project/course/entities/User.java
+++ src/main/java/com/project/course/entities/User.java
@@ -18,7 +18,7 @@ public class User implements Serializable {
     private Long id;
     private String name;
     private String email;
-    private String phoneNumber;
+    private String phone;
     private String password;
 
     @JsonIgnore
@@ -28,11 +28,11 @@ public class User implements Serializable {
     public User() {
     }
 
-    public User(Long id, String name, String email, String phoneNumber, String password) {
+    public User(Long id, String name, String email, String phone, String password) {
         this.id = id;
         this.name = name;
         this.email = email;
-        this.phoneNumber = phoneNumber;
+        this.phone = phone;
         this.password = password;
     }
 
@@ -60,12 +60,12 @@ public class User implements Serializable {
         this.email = email;
     }
 
-    public String getPhoneNumber() {
-        return phoneNumber;
+    public String getPhone() {
+        return phone;
     }
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 
     public String getPassword() {
